af72331d6b38aafb227f4cefc8d43189428c7ecb
adamscarlat <adamscarlat@gmail.com>
2021-04-19 13:48:39 -0400

    added texture loading example for cube

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -2,17 +2,32 @@ import {
   BoxBufferGeometry,
   Mesh,
   MeshBasicMaterial,
+  TextureLoader,
   MeshStandardMaterial,
 } from "../../../vendor/three/build/three.module.js";
 
-const radiansPerSecond = 30 * (Math.PI / 180);
+function createMaterial() {
+  // create a texture loader.
+  const textureLoader = new TextureLoader();
+
+  // load a texture
+  const texture = textureLoader.load("/assets/textures/uv-test-col.png");
+
+  // create a "standard" material using
+  // the texture we just loaded as a color map
+  const material = new MeshStandardMaterial({
+    map: texture,
+  });
+
+  return material;
+}
 
 function createCube() {
   // create a geometry
   const geometry = new BoxBufferGeometry(2, 2, 2);
 
   // create a default (white) Basic material
-  const material = new MeshStandardMaterial({ color: "peachpuff" });
+  const material = createMaterial();
 
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
@@ -21,7 +36,7 @@ function createCube() {
   cube.position.set(-5, 0, 0);
   cube.rotation.set(-0.5, -0.1, 0.8);
 
-  const degreesPerSeconds = 3.6;
+  const degreesPerSeconds = 12;
   const radiansPerSecond = degreesPerSeconds * (Math.PI / 180);
 
   // this method will be called once per frame
